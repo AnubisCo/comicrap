db63ab31daf69b6da2a8360342e7c42cfcdfefd6
Josyah <mjosyah@gmail.com>
2020-10-14 19:27:43 -0500

    More
    MORE LOGIN

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
-
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'; 
+import ModalHeader from "react-bootstrap/esm/ModalHeader";
 
 function Login() {
   const [show, setShow] = React.useState(false);
@@ -20,21 +22,31 @@ function Login() {
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
+          <ModalHeader> <h4>Login</h4> </ModalHeader>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Remember Me!" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
-          <button onClick={handleClose}>Close</button>
+          Don't have an account? Click here to create one!
         </Modal.Footer>
       </Modal>
     </>
