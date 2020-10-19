32fc1ef54618006bf47fa2148cc19bd4328e9681
zach morrison <morrisonizach@gmail.com>
2020-10-19 16:25:42 -0500

    nav edits

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,12 +1,34 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-// import line from "./../img/line.png";
-// import logo from "./../img/logo.png";
+import line from "./../img/line.png";
+import logo from "./../img/logo.png";
 import './Nav.css';
 
-export default function Nav(props) {
+function Nav(props) {
+    const [isNavVisible, setIsNavVisible] = React.useState(true);
+
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
+
     return (
-        <>
-        </>
+        <header className="header pb-3">
+            <img src={line} className="line" alt="line" />
+            <button onClick={toggleNav} className="toggleNav">
+                toggle
+            </button>
+            <img src={logo} className="logo" alt="logo" />
+            {isNavVisible && (
+                <nav className="nav">
+                    <a href="/">news</a>
+                    <a href="/">creators</a>
+                    <a href="/">comics</a>
+                    <a href="/">market</a>
+                    <button className="account">login</button>
+                </nav>
+            )}
+        </header>
     );
 }
+
+export default Nav;
