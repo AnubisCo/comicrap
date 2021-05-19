9b1d75d8643eed8d89c80c47678e2bdae309d63a
zach morrison <morrisonizach@gmail.com>
2021-05-19 03:24:30 -0500

    small styles

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,7 @@
 //  home page
 //  author: ziem
 import React from "react";
-import Typist from "react-typist";
-import "react-typist/dist/Typist.css";
 import 'react-slideshow-image/dist/styles.css';
-import Button from "@material-ui/core/Button";
 
 //  importing styles
 import "./Home.css";
@@ -16,7 +13,6 @@ import Comics from "./Comics";
 import Merch from "./Merch";
 
 //  importing components for site
-import Slideshow from "./../components/Slideshow";
 import Nav from "./../components/Nav";
 
 //  importing sample images
@@ -32,25 +28,8 @@ const imgs = [img1, img2, img3, img4, img5];
 function HomeDisplay(props) {
     return (
         <div id="homeHeroSection">
-            <Slideshow location="home" images={imgs} />
-            <div className="shadeScreen"></div>
-            <div className="container row my-auto" style={{ zIndex: "10", }}>
-                <div className="col-md-8">
-                    <h1 style={{ color: "white", margin: "0"}}>
-                        <Typist>
-                            <Typist.Delay ms={1000} />
-                            Comics are made.
-                            <br />
-                            <Typist.Delay ms={1500} />
-                            Together.
-                        </Typist>
-                    </h1>
-                </div>
-                <div className="col-xs-4">
-                    <Button color="primary">
-                        hello
-                    </Button>
-                </div>
+            <div className="container row my-auto" style={{ zIndex: "10"}}>
+                
             </div>
         </div>
     );
