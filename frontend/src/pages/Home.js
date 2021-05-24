174356acaedbe93ba8314e6bf04d9b8d68443b33
zach <ziemboy9@gmail.com>
2021-05-24 04:14:49 -0500

    pushing some simple animation changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -28,17 +28,21 @@ import "./Home.css";
 //  description: this function defines the hero section of the home page 
 function HomeDisplay(props) {
     const variants = {
-        hidden: { opacity: 0 },
-        visible: { opacity: 1, transition: {duration: 0.5} },
+        init: { opacity: 0, scale: 2 },
+        anime: { opacity: 1, scale: 0.75, transition: { duration: 1 } },
+    }
+    const circle_variants = {
+        init: {cx: "50%", cy: "50%"},
+        anime: {cx: "150px", cy: "150px", transition: { duration: 5 } }
     }
     return (
-        <>
-            <motion.svg width="100vw"
-            height="100vh" initial="hidden" animate="visible" variants={variants}>
-                <circle stroke="#4b2584" fill="#f17e3a" strokeWidth="1%" cx="50%" cy="50%" r="75px" />
+        <motion.svg width="100vw"
+        height="100vh" initial="init" animate="anime" variants={variants}>
+            <motion.circle variants={circle_variants} initial="init" animate="anime" stroke="#4b2584" fill="#f17e3a" strokeWidth="1%" r="75px" />
+            <motion.svg>
                 <path stroke="#f17e3a" fill="#4b2584" viewBox="0 0 150 200" strokeWidth="1%" d="M 0 150 L 75 0 150 150 75 200 Z"/>
             </motion.svg>
-        </>
+        </motion.svg>
     );
 }
 
