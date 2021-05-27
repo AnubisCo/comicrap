f2687bbc4788c217c91814c1dbc8eac09dc7a224
zach <ziemboy9@gmail.com>
2021-05-27 05:10:58 -0500

    completely reworking the home page/site theme

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -27,22 +27,44 @@ import "./Home.css";
 //  function HomeDisplay( props )
 //  description: this function defines the hero section of the home page 
 function HomeDisplay(props) {
-    const variants = {
-        init: { opacity: 0, scale: 2 },
-        anime: { opacity: 1, scale: 0.75, transition: { duration: 1 } },
-    }
-    const circle_variants = {
-        init: {cx: "50%", cy: "50%"},
-        anime: {cx: "150px", cy: "150px", transition: { duration: 5 } }
-    }
+    const variants = [
+        { scale: 3, x: "50%", y: "50%" },
+        { opacity: 1, scale: 1, x: "25%", y: "15%", transition: { delay: 1, duration: 1.5 } }
+    ]
+    const hill_variants = [
+        { overflow: "visible", position: "absolute", bottom: "0", height: "80%", width: "100%", zIndex: 1, backgroundColor: "#bdf881", borderTopRightRadius: "50% 25%", borderTopLeftRadius: "50% 25%" },
+        { height: "30%", boxShadow:"0.1rem 0.1rem 0.1rem #bdf881", transition: { delay: 1, duration: 1.5 } }
+    ]
     return (
-        <motion.svg width="100vw"
-        height="100vh" initial="init" animate="anime" variants={variants}>
-            <motion.circle variants={circle_variants} initial="init" animate="anime" stroke="#4b2584" fill="#f17e3a" strokeWidth="1%" r="75px" />
-            <motion.svg>
-                <path stroke="#f17e3a" fill="#4b2584" viewBox="0 0 150 200" strokeWidth="1%" d="M 0 150 L 75 0 150 150 75 200 Z"/>
-            </motion.svg>
-        </motion.svg>
+        <>
+            <svg width="100vw" height="100vh" style={{ overflow: "visible", zIndex: 0, position: "absolute", backgroundColor: "#121211"}}>
+                <motion.g initial={variants[0]} animate={variants.slice(1)} variants={variants} style={{ overflow: "visible" }}>
+                    <circle id="sun" stroke="#bdf881" fill="#1179cc" strokeWidth="1%" r="75px" />
+                    <text width="100%" style={{ transform: "translate3d(0,0,0);", color: "white"}} alignmentBaseline="top" xlinkHref="#sun">
+                        <textPath>
+                            weseethesun.com
+                        </textPath>
+                    </text>
+                    <svg x="-77.5">
+                        <path stroke="#1179cc" fill="#bdf881" viewBox="0 0 155 205" strokeWidth="1%" d="M 5 155 L 80 5 155 155 80 205 Z"/>
+                    </svg>
+                </motion.g>
+            </svg>
+            <motion.div initial={hill_variants[0]} animate={hill_variants.slice(1)} variants={hill_variants}>
+                <div className="row h-100" style={{ display: "flex", alignItems: "center", justifyItems: "center" }}>
+                    <div className="col-6 text-center">
+                        <a href="www.google.com">
+                            <h1>our work</h1>
+                        </a>
+                    </div>
+                    <div className="col-6 text-center">
+                        <a href="www.google.com">
+                            <h1>news</h1>
+                        </a>
+                    </div>
+                </div>
+            </motion.div>
+        </>
     );
 }
 
